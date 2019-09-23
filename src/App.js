d368123bdd26091102e547849a50d6ca1b01d032
ignacio.ortega <acernacho@gmail.com>
2019-09-23 16:51:46 +0200

    3.3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super() 
+    this.state = { mouseX: 0, mouseY: 0}
+  }
+
+  handleMouseMove = (e) => {
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+
   handleCLick (e) {
     console.log(e);
     alert('hola')
@@ -13,6 +23,10 @@ class App extends Component {
       <div className="App">
         <h4>Eventos</h4>
         <button onClick={this.handleCLick}>Hola</button>
+        <div onMouseMove={this.handleMouseMove}
+          style={{border: '1px solid #000', marginTop: 10, padding: 10}}>
+            <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
