f28fccb17f962096c31c8161abe4b13f4d440831
ignacio.ortega <acernacho@gmail.com>
2019-09-23 14:55:52 +0200

    3.2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  handleCLick (e) {
+    console.log(e);
+    alert('hola')
+  }
+
   render () {
     return (
       <div className="App">
         <h4>Eventos</h4>
-        <button onClick={() => alert('hola')}>Hola</button>
+        <button onClick={this.handleCLick}>Hola</button>
       </div>
     );
   }
