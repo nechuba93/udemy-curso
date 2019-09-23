29de8dabe0a1b1de7a6d6234588199de3e230a6f
ignacio.ortega <acernacho@gmail.com>
2019-09-23 17:35:40 +0200

    4.0

--- src/App.js
+++ src/App.js
@@ -1,62 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 
+class Box extends Component {
+  render () {
+    return (
+      <div style={{border: '1px solid #09f', margin: 5, padding: 5}}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
-
-  constructor () {
-    super() 
-    this.state = {
-      inputName: '',
-      inputTwitter: '@',
-      inputTerms: true
-    }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state);
-  }
-
-  handleChange = (e) => {
-    console.log(e.target.checked);
-    this.setState({inputTerms: e.target.checked})
-  }
 
   render () {
     return (
       <div className="App">
-        <h4>Formularios</h4>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <label htmlFor='name'>Nombre: </label>
-            <input
-              id='name'
-              name='username'
-              onChange={e => this.setState({inputName: e.target.value})}
-              placeholder='Introduce el nombre'
-              ref={inputElement => this.inputName = inputElement}
-              value={this.state.inputName}>
-            </input>
-          </p>
-          <p>
-            <label htmlFor='twitter'>Twitter: </label>
-            <input
-              id='twitter'
-              name='twitterAccount'
-              onChange={e => this.setState({inputTwitter: e.target.value})}
-              placeholder='Introduce el nombre'
-              value={this.state.inputTwitter}>
-            </input>
-          </p>
-          <p>
-            <label htmlFor='twitter'>
-            <input 
-              checked={this.state.inputTerms}
-              type='checkbox' onChange={this.handleChange}></input>
-            Aceptar términos </label>
-          </p>
-          <button>Enviar</button>
-        </form>
+        <h4>Children props</h4>
+        <Box>Hola!</Box>
+        <Box>Adio!</Box>
       </div>
     );
   }
