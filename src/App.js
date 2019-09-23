0e04eb3075164aef4af3e43a029effb315d1ea2a
ignacio.ortega <acernacho@gmail.com>
2019-09-23 13:24:08 +0200

    1.8

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class Contador extends Component {
   constructor () {
     super()
     this.state = { contador: 1}
+    setInterval(() => {
+      this.setState({contador: this.state.contador + 1})
+    }, 1000);
   }
 
   render () {
