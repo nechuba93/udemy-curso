9204521d9facc5405f19f58425084a2a8b01c877
ignacio.ortega <acernacho@gmail.com>
2019-09-23 14:39:26 +0200

    3.1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React, {Component} from 'react';
-import cars from './data/cars.json'
 import './App.css';
-
-class CarItem extends Component {
-  render () {
-    const { car, id } = this.props 
-
-    return (
-      <li>
-        <p>Key: {id}</p>
-        <p><strong>Nombre:</strong> {car.name}</p>
-        <p><strong>Marca:</strong> {car.company}</p>
-      </li>
-    )
-  }
-}
-
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h4>Trabajando con listas</h4>
-        <ul>
-          {
-            cars.map(car => {
-              return <CarItem id={car.id} key={car.id} car={car} />
-            })
-          }
-        </ul>
+        <h4>Eventos</h4>
+        <button onClick={() => alert('hola')}>Hola</button>
       </div>
     );
   }
