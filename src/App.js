c533adca647955d0ccab5bf929977e4707a318ae
ignacio.ortega <acernacho@gmail.com>
2019-09-23 17:40:13 +0200

    4.1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 class Box extends Component {
   render () {
     return (
-      <div style={{border: '1px solid #09f', margin: 5, padding: 5}}>
+      <div style={{border: '1px solid #000', margin: 5, padding: 5}}>
         {this.props.children}
       </div>
+    )
+  }
+}
+
+class Article extends Component {
+  render () {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p><em>Escrito por {this.props.author}</em></p>
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
     )
   }
 }
@@ -17,8 +32,14 @@ class App extends Component {
     return (
       <div className="App">
         <h4>Children props</h4>
-        <Box>Hola!</Box>
-        <Box>Adio!</Box>
+        <Article
+          author='Nacho'
+          date={new Date().toLocaleDateString()}
+          title='Articulo de props'
+        >
+          <p>Contenido contenido contenido contenido</p>
+          <b>Pie pie pie pie</b>
+        </Article>
       </div>
     );
   }
