c31afbad58010d2e2de6bd6772ceb65502ed955f
ignacio.ortega <acernacho@gmail.com>
2019-09-24 10:07:10 +0200

    4.5

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
-
-class Box extends Component {
-  render () {
-    return (
-      <div style={{border: '1px solid #000', margin: 5, padding: 5}}>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-class Article extends Component {
-
-  static propTypes = {
-    author: PropTypes.string.isRequired
-  }
-
-  render () {
-    const {author, children, date, title } = this.props
-    return (
-      <section>
-        <h2>{title}</h2>
-        {author && <p><em>Escrito por {author}</em></p>}
-        <Box>{date}</Box>
-        <article>
-          {children}
-        </article>
-      </section>
-    )
-  }
-}
 
 class App extends Component {
+  constructor (props) {
+    super (props) //COnstructor de Component
+    // Inicializamos el state
+    this.state = { mensajeInicial: 'mensaje inicial' }
+
+  }
+
+  handleClick = () => {
+    this.setState({mensajeInicial: 'cambiado'})
+  }
 
   render () {
+    console.log('render');
     return (
       <div className="App">
         <h4>Children props</h4>
-        <Article
-          author='Nacho'
-          date={new Date().toLocaleDateString()}
-          title='Articulo de props'
-        >
-          <p>Contenido contenido contenido contenido</p>
-          <b>Pie pie pie pie</b>
-        </Article>
+        {this.state.mensajeInicial} 
+        <button onClick={this.handleClick}>
+          Cambiar Mensaje
+        </button>
       </div>
     );
   }
